47851a3a98ae5a940da21417c83e9597937b1ef0
Sayara Kaylaine <159396381+sayarakaylaine@users.noreply.github.com>
2024-02-28 09:49:37 -0400

    Add files via upload

---
+++ AppProjetoAluno/Models/Aluno.cs
@@ -0,0 +1,26 @@
+﻿namespace AppProjetoAluno.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; } 
+        public string? Nome { get; set;}
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+
+        public double Media { get; set;}
+        public string? Situacao { get; set; }
+
+        public void CalcularMedia()
+        {
+            Situacao = "Reprovado";
+            Media = (Nota1 + Nota2) / 2;
+            if (Media >= 60) 
+            {
+                Situacao = "Aprovado";
+            }
+        }
+
+    }
+        
+}
+
